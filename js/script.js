578a5f35cc25e4d3a7007e8060c53bfc82ea5c42
Monie Sharma <munish2870@hotmail.com>
2018-08-13 16:05:01 +0200

    Self randomizing done
    SetInterval function attached

--- js/script.js
+++ js/script.js
@@ -6,49 +6,50 @@ const quotes = [
     quote:
       "Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.",
     source: "Norman Vincent Peale",
-    category: "Motivation",
-    citation: "Facebook"
+    category: "Sad",
+    citation: "Facebook",
+    year: "2017"
   },
   {
     quote: "If you can dream it, you can do it.”",
     source: "Walt Disney",
     category: "Motivation",
     year: "2010",
-    citation: "Twitter"
+    citation: "Some Website"
   },
   {
     quote:
       "Where there is a will, there is a way. If there is a chance in a million that you can do something, anything, to keep what you want from ending, do it. Pry the door open or, if need be, wedge your foot in that door and keep it open.",
     source: "Pauline Kael",
-    category: "Motivation"
-    // citation: "Twitter"
+    category: "Sad"
+
   },
   {
     quote:
       "Do not wait; the time will never be ‘just right.’ Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along.",
     source: "George Herbert",
-    category: "Motivation"
-    // citation: "Twitter"
+    category: "Humor"
+
   },
   {
     quote:
       "Press forward. Do not stop, do not linger in your journey, but strive for the mark set before you.",
     source: "George Whitefield",
     category: "Motivation"
-    // citation: "Twitter"
+
   },
   {
     quote:
       "The future belongs to those who believe in the beauty of their dreams.",
     source: "Eleanor Roosevelt",
-    category: "Motivation"
-    // citation: "Twitter"
+    category: "Humor"
+
   },
   {
     quote: "Aim for the moon. If you miss, you may hit a star.",
     source: "W. Clement Stone",
-    category: "Motivation"
-    // citation: "Twitter"
+    category: "Self-Motivation"
+
   },
   {
     quote: "Don’t watch the clock; do what it does. Keep going.",
@@ -80,8 +81,6 @@ const getRandomQuote = quote => {
 // Create the printQuote funtion and name it printQuote
 const printQuote = quote => {
   let myDiv = document.getElementById("quote-box");
-  // let quoteCitiation = document.querySelector("p.source");
-  // console.log(quoteCitiation);
 
   //getting data from the functions
   quote = getRandomQuote(quote);
@@ -92,7 +91,9 @@ const printQuote = quote => {
 
   //selecting paragraph ids and then adding data to it.
   myDiv.querySelector(".quote").innerHTML = quote.quote;
+  myDiv.querySelector(".category").innerHTML =  quote.category;
 
+//IF citation and year exists then it will add to the browser
   if(quote.citation){
  myDiv.querySelector(".source").innerHTML = quote.source + ", " + span.textContent
 }
@@ -103,10 +104,12 @@ else {
 }
 }
 //calling the function
-printQuote();
+myFunction = () => {
+setInterval(printQuote, 6000)
+}
 
 // This event listenerwill respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document
   .getElementById("loadQuote")
-  .addEventListener("click", printQuote, true);
+  .addEventListener("click", printQuote, myFunction())
